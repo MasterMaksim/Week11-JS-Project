34adc7020d2ee8aaf8c2c18aca50149550fe7b4c
Maksim Muravjov <mmuravjo@tktk.ee>
2022-01-04 22:37:48 +0200

    Add New JS Project

---
+++ js/randomImage.js
@@ -0,0 +1,15 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["pic1.jpeg", "pic2.jpeg", "pic3.jpg", "pic4.jpeg"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+
+    let randomIndex = Math.floor(Math.random() * images.length);
+
+    
+
+    image.src = "images/" + images[randomIndex];
+
+}
